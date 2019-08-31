00301fd3d637e4d5c002dadc7b6eb38388508bbc
Rico Nijeboer <r.nijeboer@code14.nl>
2019-08-31 08:26:35 +0200

    V2.0

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
+import Vue from 'vue';
 import MessageContainer from './MessageContainer';
 
-export default {
-    install(Vue) {
-        const messageContainer = new Vue({
-            data() {
-                return {
-                    messages: [],
-                };
+class VueFlashes {
+    constructor() {
+        if (VueFlashes.instance) {
+            return VueFlashes.instance;
+        }
+        VueFlashes.instance = this;
+
+        this._init();
+
+        return this;
+    }
+
+    _init() {
+        const el = document.createElement('div');
+        document.appendChild(el);
+        this.container = new Vue({
+            data: {
+                messages: [],
             },
             render: h => h(MessageContainer),
         });
+        this.container.$mount(el);
+    }
+}
 
-        const el = document.createElement('div');
+export const Flashes = new VueFlashes();
 
-        document.body.appendChild(el);
-
-        messageContainer.$mount(el);
-
-        Vue.prototype.$resetMessages = () => {
-            messageContainer.messages = [];
-        };
-
-        Vue.prototype.$message = (msg, type) => {
-            messageContainer.messages.push({
-                message: msg,
-                type,
-            });
-        };
-
-        Vue.prototype.$flashMessage = (msg, type, timeout = 3) => {
-            Vue.prototype.$message(msg, type);
-
-            setTimeout(() => {
-                const index = messageContainer.messages.findIndex(m => m.message === msg);
-
-                messageContainer.messages.splice(index, 1);
-            }, timeout * 1000);
-        };
+export default {
+    install(vue) {
+        vue.prototype.$flash = new VueFlashes();
     },
 };
